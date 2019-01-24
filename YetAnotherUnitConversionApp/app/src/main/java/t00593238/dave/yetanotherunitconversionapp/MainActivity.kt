b55a2a7bb02cadcebaa9d3ed89da92b4af637289
kreh <dave@ffox.land>
2019-01-24 05:40:08 -0800

    Units and things

--- YetAnotherUnitConversionApp/app/src/main/java/t00593238/dave/yetanotherunitconversionapp/MainActivity.kt
+++ YetAnotherUnitConversionApp/app/src/main/java/t00593238/dave/yetanotherunitconversionapp/MainActivity.kt
@@ -2,15 +2,25 @@ package t00593238.dave.yetanotherunitconversionapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
 import android.view.KeyEvent
 import android.view.View
 import android.widget.*
 
 
 class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
+    // TODO Degree symbol and unit on output - Done
+    // TODO unit on input - Done
+    // TODO "to <unit>" on spinner
+    // TODO onclick function to change spinner value
+    // TODO confine edittext to only number values? constrain degree symbol and unit
+    // TODO fragment and tab management for output
 
-    fun String.removeTempUnit(): Double = reversed().substring(1).reversed().toDouble()
+    fun String.removeTempUnit(): Double = reversed().substring(2).reversed().toDouble()
     var inputNumber: EditText? = null
+    var outputNumber: TextView? = null
+    var spinner: Spinner? = null
     var cSelected: Boolean = false
 
 
@@ -19,6 +29,7 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
         setContentView(R.layout.activity_main)
         inputNumber = findViewById(R.id.mainTextInput)
 
+        inputNumber?.text = SpannableStringBuilder(getString(R.string.mainTextDefaultFormat, 0.0, 'C'))
         // lambda that controls a listener to the enter key on the keyboard
         inputNumber?.setOnKeyListener(View.OnKeyListener { p0, p1, p2 ->
             if (p2?.action == KeyEvent.ACTION_DOWN && p1 == KeyEvent.KEYCODE_ENTER) {
@@ -28,8 +39,11 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
             false
         })
 
-        val spinner: Spinner = findViewById(R.id.spinner)
-        spinner.onItemSelectedListener = this
+        outputNumber = findViewById(R.id.output)
+        convertToC(0.0)
+
+        spinner = findViewById(R.id.spinner)
+        spinner?.onItemSelectedListener = this
 
         ArrayAdapter.createFromResource(
                 this,
@@ -39,35 +53,51 @@ class MainActivity : Activity(), AdapterView.OnItemSelectedListener {
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_gallery_item)
             // Apply the adapter to the spinner
-            spinner.adapter = adapter
+            spinner?.adapter = adapter
         }
     }
 
-    private fun convertToF(input: Double) {
-        val f = input * (9 / 5) + 32
-        val end: TextView? = findViewById(R.id.output)
-        end?.text = f.toString()
+    private fun getFormat(input: Double): Int {
+        return if (input.toInt().toDouble() == input) R.string.mainTextDefaultFormat else R.string.mainTextFormat
     }
 
-    private fun convertToC(input: Double) {
-        val c = (input - 32) * (5 / 9)
-        val end: TextView? = findViewById(R.id.output)
-        end?.text = c.toString()
+    private fun convertToF(input: Double): Double {
+        val f = input * (9.0 / 5.0) + 32.0
+        val format = getFormat(f)
+        outputNumber?.text = getString(format, f, 'F')
+        return f
+    }
+
+    private fun convertToC(input: Double): Double {
+        val c = (input - 32.0) * (5.0 / 9.0)
+        val format = getFormat(c)
+        outputNumber?.text = getString(format, c, 'C')
+        return c
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        spinner?.setSelection(if (cSelected) 1 else 0, true)
+        //spinner?.performItemClick(findViewById(R.id.spinner), if (cSelected) 1 else 0)
     }
 
     // Determines if using C temp or F temp to convert from (cSelected = true -> using C temp)
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        if (p2 == 0)
+        val input: Double = inputNumber?.text.toString().removeTempUnit()
+        val format = getFormat(input)
+        if (p2 == 0) {
             cSelected = true
+            inputNumber?.text = SpannableStringBuilder(getString(format, input, 'F'))
+        }
+        else {
+            cSelected = false
+            inputNumber?.text = SpannableStringBuilder(getString(format, input, 'C'))
+        }
     }
 
     fun submitInput(view: View?) {
         val input: Double = this.findViewById<EditText>(R.id.mainTextInput).text.toString().removeTempUnit()
-        if (cSelected) convertToF(input) else convertToC(input)
+        println("Input: $input")
+        println("Output: ${if (cSelected) convertToC(input) else convertToF(input)}")
 //        button.setBackgroundResource(R.drawable.btn)
 //        input.toString()
 //        String.format("%s°")
